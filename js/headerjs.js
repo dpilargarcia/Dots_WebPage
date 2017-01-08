7a6d6302029e13759a11d899a89809ef1c990c2c
dpilargarcia <pilargarcialugo@gmail.com>
2017-01-08 00:03:00 -0500

    edit4

---
+++ js/headerjs.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+ 
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 200 ){
+			$('header').addClass('header2');
+            $('contenido').addClass('contenido2');
+		} else {
+			$('header').removeClass('header2');
+            $('contenido').removeClass('contenido2');
+		}
+	});
+ 
+});
