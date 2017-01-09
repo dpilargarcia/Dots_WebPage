02f914b62aefd9685d698da48d951ed98015b6aa
dpilargarcia <pilargarcialugo@gmail.com>
2017-01-08 23:38:16 -0500

    edit5

--- js/headerdinamicojs.js
+++ js/headerdinamicojs.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
- 
-	$(window).scroll(function(){
+ 	$(window).scroll(function(){
 		if( $(this).scrollTop() > 200 ){
 			$('header').addClass('header2');
-            $('contenido').addClass('contenido2');
+            $('.contenido').addClass('contenido2');
 		} else {
 			$('header').removeClass('header2');
-            $('contenido').removeClass('contenido2');
+            $('.contenido').removeClass('contenido2');
 		}
 	});
  
